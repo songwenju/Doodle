fb26e5bbde8a116743117ee3654b4204f08fdfb2
zhaokai <zhaokai1033@163.com>
2016-04-03 00:40:36 +0800

    1.项目分包 2.增加主界面 及登录页面

--- app/src/main/java/com/zhangshuo/doodle/ui/MainActivity.java
+++ app/src/main/java/com/zhangshuo/doodle/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.zhangshuo.doodle;
+package com.zhangshuo.doodle.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.zhangshuo.doodle.R;
 
 public class MainActivity extends AppCompatActivity {
 
